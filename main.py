b93fe6494e4275d85d3469d91d2658919942bd28
CheeZePrinceZz <chidchanok.thepsoonthorn@gmail.com>
2017-06-21 16:57:47 +0900

    Add Skeleton display part

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import kinecthandler as kinecthandler
 import naocommander as naocommander
 import converter as converter
+import SkeletonDisplay as SkeletonDisplay
 
 #robotIP = "192.168.2.24"
 # robotIP = "127.0.0.1"
@@ -21,6 +22,7 @@ def kinect_test(kinect_h, nao_c):
             [r_s_roll, r_s_pitch, r_e_roll, r_e_yaw, r_w_yaw] = converter.get_right_arm(res[i][0], res[i][1])
             [l_s_roll, l_s_pitch, l_e_roll, l_e_yaw, l_w_yaw] = converter.get_left_arm(res[i][0], res[i][1])
             h_pitch = converter.get_head(res[i][0])
+            h_yaw = converter.get_head2(res[i][0])
             [r_hand, l_hand] = converter.get_hands(res[i][2])
             nao_c.move_robot(right_shoulder_roll=r_s_roll, right_shoulder_pitch=r_s_pitch,
                              right_elbow_roll=r_e_roll, right_elbow_yaw=r_e_yaw,
@@ -28,11 +30,12 @@ def kinect_test(kinect_h, nao_c):
                              left_shoulder_roll=l_s_roll, left_shoulder_pitch=l_s_pitch,
                              left_elbow_roll=l_e_roll, left_elbow_yaw=l_e_yaw,
                              left_wrist_yaw=l_w_yaw,
-                             head_pitch=h_pitch, right_hand=r_hand, left_hand=l_hand,
+                             head_pitch=h_pitch, head_yaw=h_yaw, right_hand=r_hand, left_hand=l_hand,
                              pfractionmaxspeed=0.4)
 
 
 if __name__ == '__main__':
+    SkeletonDisplay.BodyGameRuntime()
     _sensor = kinecthandler.KinectHandler()
     _avatar = naocommander.NAOCommander(robotIP, PORT)
     kinect_test(_sensor, _avatar)
